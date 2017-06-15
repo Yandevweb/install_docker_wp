4aa0571a05e333d7426afc0aaa1fdb7f85472031
Yandevweb <yannick.jeanjean.pro@gmail.com>
2017-06-15 12:16:35 +0200

    Mise à jour du script afin de le rendre fonctionnel

--- install_wp.sh
+++ install_wp.sh
@@ -1,33 +1,30 @@
+#!/bin/bash
+# Installation de Docker
+
 cmdProxy='command'
 command type -f sudo &> /dev/null && cmdProxy='sudo'
 
-# Installation de Docker
+# install MySQL
+${cmdProxy} docker run --name mysql -e MYSQL_ROOT_PASSWORD=0000 -d mysql:latest
+
+
+# récup de l'id de l'image
+idMysql="$(docker ps -aqf "name="mysql)"
 
 ${cmdProxy} docker build -f imgApache -t apache .
 
-#${cmdProxy} docker run -d -p 4000:80 apache
-
-# install MySQL
-${cmdProxy} docker run --name mysql -e MYSQL_ROOT_PASSWORD=0000 -d mysql:latest .
-
-# récup de l'id de l'image
-
-${cmdProxy} rm id_mysql.txt
-${cmdProxy} docker ps | grep mysql > id_mysql.txt
-idMysql=${cmdProxy}cut -c1-12 id_mysql.txt
-
-echo "$idMysql"
-
 # link des containers apache et mysql
-${cmdProxy} docker run --name apache --link ${idMysql} -p 4000:80 -d apache .
+docker run --name apache --link ${idMysql} -p 4000:80 -d apache
+
+echo "======================================"
+echo "${idMysql}"
+echo "======================================"
 
 # récup de l'id de l'image
-
-${cmdProxy} rm id_apache.txt
-${cmdProxy} docker ps | grep apache > id_apache.txt
-idApache=${cmdProxy}cut -c1-12 id_apache.txt
-
-echo "$idApache"
+idApache="$(docker ps -aqf "name="apache)"
+echo "======================================"
+echo "${idApache}" 
+echo "======================================"
 
 
 ${cmdProxy} docker exec -it ${idApache} bash
